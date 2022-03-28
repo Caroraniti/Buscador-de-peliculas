780fe313e3ff443ce6893ecbd157ccfb8693fe92
Carolina Belen Raniti <craniti@buenosaires.gob.ar>
2022-03-28 14:09:43 -0300

    agrego el componente Main y la ruta

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
+import Main from "./components/Main"
 
 const App = () => {
   return (
 
     <BrowserRouter>
       <Nav>
-      <Routes>
-        <Route>
-        
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={< Main />} />
+
+        </Routes>
 
       </Nav>
-    
-    
+
+
     </BrowserRouter>
   )
 }
