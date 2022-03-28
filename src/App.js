a81845cc9e81f91270709272ab353bf6cc0ee87e
Carolina Belen Raniti <craniti@buenosaires.gob.ar>
2022-03-28 14:18:03 -0300

    Agrego componente ultimos lanzamientos y la ruta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Main from "./components/Main"
+import UltimosLanzamientos from "./components/UltimosLanzamientos"
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Nav>
         <Routes>
           <Route path="/" element={< Main />} />
-
+          <Route path="/UltimosLanzamientos" element={<UltimosLanzamientos />} />
         </Routes>
 
       </Nav>
