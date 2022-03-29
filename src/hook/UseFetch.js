158660593fe574ab7275fd19f48fdffb9f690d5b
Carolina Belen Raniti <craniti@buenosaires.gob.ar>
2022-03-29 16:00:38 -0300

    agrego useeffect y usestate

---
+++ src/hook/UseFetch.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const UseFetch = (url) => {
+    return (
+        const [peliculas, setPeliculas] =useState([])
+    
+            useEffect (() => {
+                
+            })
+    
+        )
+
+
+}
+
+
+
+export default UseFetch;
